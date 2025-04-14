d9d18ff4dff036dbb7fd04d55b3a8c854a690bdc
ryans <ryans@jlab.org>
2025-04-14 09:31:40 -0400

    schema

--- container/oracle/initdb.d/03_default_data.sql
+++ container/oracle/initdb.d/03_default_data.sql
@@ -8,3 +8,6 @@ insert into ARM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) value
 insert into ARM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_CDN', 'N', 'BOOLEAN', 'Smoothness weblib resources from CDN.  Defaults to No = serve files locally. CDN is for minified/combined files on shared Content Delivery Network (CDN) server - Nice for when multiple apps use same resources to have warm cache.', 'CDN', 1);
 insert into ARM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_SERVER', 'ace.jlab.org/cdn', 'STRING', 'Host name and port of content delivery network host for shared smoothness resources. Only used if SMOOTHNESS_LOCATION=CDN', 'CDN', 2);
 insert into ARM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_VERSION', '4.11.0', 'STRING', 'Version of smoothness lib on CDN.  Only used if SMOOTHNESS_LOCATION=CDN', 'CDN', 3);
+
+-- SOFTWARE
+insert into ARM_OWNER.SOFTWARE (SOFTWARE_ID, NAME, CODE_URL, DOC_URL, MAINTAINER_USERNAME_CSV) values (SOFTWARE_ID.nextval, 'ARM', 'https://github.com/jeffersonlab/arm', 'https://github.com/jeffersonlab/arm', 'ryans');
