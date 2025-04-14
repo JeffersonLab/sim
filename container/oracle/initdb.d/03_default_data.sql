8c8ec0e84548eb987b06a25ad413d3c7ae234cdb
ryans <ryans@jlab.org>
2025-04-14 09:49:32 -0400

    rename arm to sim

--- container/oracle/initdb.d/03_default_data.sql
+++ container/oracle/initdb.d/03_default_data.sql
@@ -10,4 +10,4 @@ insert into SIM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) value
 insert into SIM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_VERSION', '4.11.0', 'STRING', 'Version of smoothness lib on CDN.  Only used if SMOOTHNESS_LOCATION=CDN', 'CDN', 3);
 
 -- SOFTWARE
-insert into SIM_OWNER.SOFTWARE (SOFTWARE_ID, NAME, CODE_URL, DOC_URL, MAINTAINER_USERNAME_CSV) values (SOFTWARE_ID.nextval, 'SIM', 'https://github.com/jeffersonlab/sim', 'https://github.com/jeffersonlab/sim', 'ryans');
+insert into SIM_OWNER.SOFTWARE (SOFTWARE_ID, NAME, CODE_URL, DOC_URL, MAINTAINER_USERNAME_CSV) values (SIM_OWNER.SOFTWARE_ID.nextval, 'SIM', 'https://github.com/jeffersonlab/sim', 'https://github.com/jeffersonlab/sim', 'ryans');
