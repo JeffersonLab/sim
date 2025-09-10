6dbf9436bc13da6c1411ec3b6e2885bfc54c23e7
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-09-10 17:02:32 -0400

    Use native queries to update join table
    Fixes #25

--- src/main/java/org/jlab/sim/business/service/SoftwareTopicService.java
+++ src/main/java/org/jlab/sim/business/service/SoftwareTopicService.java
@@ -3,11 +3,11 @@ package org.jlab.sim.business.service;
 import jakarta.annotation.security.PermitAll;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
 import org.jlab.sim.persistence.entity.Software;
 import org.jlab.sim.persistence.entity.SoftwareTopic;
-import org.jlab.sim.persistence.entity.SoftwareTopicId;
 import org.jlab.sim.persistence.entity.Topic;
 import org.jlab.smoothness.business.service.JPAService;
 
@@ -31,28 +31,31 @@ public class SoftwareTopicService extends JPAService<SoftwareTopic> {
     return query.getResultList();
   }
 
-  @PermitAll
-  public void set(Software software, String[] topicNameArray) {
+  private void removeAllTopics(Software software) {
+    Query q = em.createNativeQuery("delete from software_topic where software_id = ?");
 
-    // First, we remove / clear any existing
-    List<SoftwareTopic> softwareTopics = findBySoftware(software);
+    q.setParameter(1, software.getSoftwareId());
 
-    if (softwareTopics != null) {
-      for (SoftwareTopic st : softwareTopics) {
-        remove(st);
-      }
-    }
+    q.executeUpdate();
+  }
 
-    // Next, we add the new ones
+  private void addSoftwareTopic(Software software, Topic topic) {
+    Query q =
+        em.createNativeQuery("insert into software_topic (software_id, topic_id) values (?, ?)");
+
+    q.setParameter(1, software.getSoftwareId());
+    q.setParameter(2, topic.getTopicId());
+
+    q.executeUpdate();
+  }
+
+  private void addTopics(Software software, String[] topicNameArray) {
     if (topicNameArray != null) {
       for (String topicName : topicNameArray) {
 
         if (topicName == null || topicName.isBlank()) {
           continue;
         }
-
-        SoftwareTopic st = new SoftwareTopic();
-        st.setSoftware(software);
 
         Topic topic = topicService.findByName(topicName);
 
@@ -63,15 +66,18 @@ public class SoftwareTopicService extends JPAService<SoftwareTopic> {
           em.flush();
         }
 
-        st.setTopic(topic);
-
-        SoftwareTopicId SoftwareTopicId =
-            new SoftwareTopicId(software.getSoftwareId(), topic.getTopicId());
-
-        st.setSoftwareTopicId(SoftwareTopicId);
-
-        create(st);
+        addSoftwareTopic(software, topic);
       }
     }
+  }
+
+  @PermitAll
+  public void set(Software software, String[] topicNameArray) {
+
+    // First, we remove / clear any existing
+    removeAllTopics(software);
+
+    // Next, we add the new ones
+    addTopics(software, topicNameArray);
   }
 }
